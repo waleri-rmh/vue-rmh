edf943c8ca00749c9e689e2ba4c81d481bc5ad33
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-11 19:00:49 +0200

    update: finished component rmh-checkbox

--- src/components/rmh-checkbox/rmh-checkbox.vue
+++ src/components/rmh-checkbox/rmh-checkbox.vue
@@ -16,9 +16,15 @@ export default {
   },
 
   props: {
-    value: {
+    model: [String, Boolean, Object, Number, Array],
+    id: {
       type: String,
-      default: ''
+      default: null
+    },
+    name: [String, Number],
+    value: {
+      type: [String, Boolean, Object, Number],
+      default: 'on'
     },
     label: {
       type: String,
@@ -28,19 +34,29 @@ export default {
       type: String,
       default: ''
     },
-    bordered: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    required: {
       type: Boolean,
       default: false
     }
   },
 
+  model: {
+    prop: 'model',
+    event: 'change'
+  },
+
   data: () => ({
-    localValue: ''
+    uId: null
   }),
 
   mounted () {
     this.localValue = this.value
     this.$refs.field.inputMounted(this.value)
+    this.uId = this.id ? this.id : 'rmh-checkbox-' + this._uid
   },
 
   computed: {
@@ -48,6 +64,34 @@ export default {
       let l = { ...this.$listeners }
       delete l.input
       return l
+    },
+
+    isSelected () {
+      if (this.isModelArray) {
+        return this.model.includes(this.value)
+      }
+
+      if (this.isModelBoolean && this.value === 'on') {
+        return this.model
+      }
+
+      return this.model === this.value
+    },
+
+    isModelArray () {
+      return Array.isArray(this.model)
+    },
+
+    isModelBoolean () {
+      return typeof this.model === 'boolean'
+    },
+
+    checkClasses () {
+      return {
+        'checked': this.isSelected,
+        'disabled': this.disabled,
+        'required': this.required
+      }
     }
   },
 
@@ -61,12 +105,56 @@ export default {
   },
 
   methods: {
+    removeItemFromModel (newModel) {
+      const index = newModel.indexOf(this.value)
+
+      if (index !== -1) {
+        newModel.splice(index, 1)
+      }
+    },
+
+    handleArrayCheckbox () {
+      const newModel = this.model
+
+      if (!this.isSelected) {
+        newModel.push(this.value)
+      } else {
+        this.removeItemFromModel(newModel)
+      }
+
+      this.$emit('change', newModel)
+    },
+
+    handleStringCheckbox () {
+      if (!this.isSelected) {
+        this.$emit('change', this.value)
+      } else {
+        this.$emit('change', null)
+      }
+    },
+
+    handleBooleanCheckbox () {
+      this.$emit('change', !this.model)
+    },
+
     focus (e) {
       this.$refs.field.focus(e)
     },
 
     blur (e) {
       this.$refs.field.blur(e)
+    },
+
+    toggle (e) {
+      if (!this.disabled) {
+        if (this.isModelArray) {
+          this.handleArrayCheckbox()
+        } else if (this.isModelBoolean) {
+          this.handleBooleanCheckbox()
+        } else {
+          this.handleStringCheckbox()
+        }
+      }
     }
   }
 }
