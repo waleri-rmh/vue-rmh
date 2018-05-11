99b234e5943805fc7131121516d9f931f812f05c
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-11 15:53:35 +0200

    update: rework module loading & initializing plugin install

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import 'es6-promise/auto'
 
-import rmhField from './src/components/rmh-field/rmh-field.vue'
-import rmhSelect from './src/components/rmh-select/rmh-select.vue'
-import rmhOption from './src/components/rmh-option/rmh-option.vue'
-import rmhTextbox from './src/components/rmh-textbox/rmh-textbox.vue'
-import rmhButton from './src/components/rmh-button/rmh-button.vue'
-import rmhIcon from './src/components/rmh-icon/rmh-icon.vue'
+const modules = require.context('./src/components/', true, /\.vue$/)
 
 const rmhPlugin = {
-  install (Vue, options) {
-    Vue.component(rmhField.name, rmhField),
-    Vue.component(rmhSelect.name, rmhSelect),
-    Vue.component(rmhOption.name, rmhOption),
-    Vue.component(rmhTextbox.name, rmhTextbox),
-    Vue.component(rmhButton.name, rmhButton),
-    Vue.component(rmhIcon.name, rmhIcon)
+  install (Vue) {
+    modules.keys().forEach(key => {
+      const module = modules(key).default
+      Vue.component(module.name, module)
+    })
   }
 }
 
