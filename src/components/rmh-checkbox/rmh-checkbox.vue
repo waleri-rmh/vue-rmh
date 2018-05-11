bfec799826dcf593868b950e8e8b060e22d10f95
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-11 15:52:44 +0200

    add: component rmh-checkbox (empty)

---
+++ src/components/rmh-checkbox/rmh-checkbox.vue
@@ -0,0 +1,75 @@
+<template src="./rmh-checkbox.html"></template>
+
+<script>
+import component from '../../mixins/component'
+import rmhField from '../../components/rmh-field/rmh-field'
+import rmhIcon from '../../components/rmh-icon/rmh-icon'
+
+export default {
+  name: 'rmh-checkbox',
+
+  mixins: [component],
+
+  components: {
+    rmhField,
+    rmhIcon
+  },
+
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    bordered: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data: () => ({
+    localValue: ''
+  }),
+
+  mounted () {
+    this.localValue = this.value
+    this.$refs.field.inputMounted(this.value)
+  },
+
+  computed: {
+    listeners () {
+      let l = { ...this.$listeners }
+      delete l.input
+      return l
+    }
+  },
+
+  watch: {
+    localValue (val) {
+      this.$emit('input', val)
+    },
+    value (val) {
+      this.localValue = val
+    }
+  },
+
+  methods: {
+    focus (e) {
+      this.$refs.field.focus(e)
+    },
+
+    blur (e) {
+      this.$refs.field.blur(e)
+    }
+  }
+}
+</script>
+
+<style src="./rmh-checkbox.styl" lang="stylus"></style>
