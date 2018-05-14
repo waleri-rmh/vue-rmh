d908fea096bd675d0e984ebe9f094914f110ddc3
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 20:40:59 +0200

    add: component rmh-content

---
+++ src/components/layout/rmh-content/rmh-content.vue
@@ -0,0 +1,17 @@
+<template src="./rmh-content.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-content',
+
+  computed: {
+    hasDefaultSlot () {
+      return !!this.$slots.default
+    }
+  }
+}
+</script>
+
+<style src="./rmh-content.styl" lang="stylus"></style>
