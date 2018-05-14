a56ca6855bb4426a0effab16ce3dcc8cdcfff997
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 20:40:46 +0200

    add: component rmh-app

---
+++ src/components/layout/rmh-app/rmh-app.vue
@@ -0,0 +1,38 @@
+<template src="./rmh-app.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-app',
+
+  props: {
+    fixedToolbar: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data: () => ({
+    menu: null
+  }),
+
+  computed: {
+    hasDefaultSlot () {
+      return !!this.$slots.default
+    },
+
+    classes () {
+      return {
+        'fixed-toolbar': this.fixedToolbar
+      }
+    }
+  },
+
+  mounted () {
+    this.menu = this.$children.filter(child => { return child.$options._componentTag === 'rmh-drawer' })[0]
+  }
+}
+</script>
+
+<style src="./rmh-app.styl" lang="stylus"></style>
