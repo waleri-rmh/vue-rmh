5646806f95a0d712994533610047742c67289a51
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 15:28:52 +0200

    add: rmh-toolbar component

---
+++ src/components/layout/rmh-container/rmh-container.vue
@@ -0,0 +1,17 @@
+<template src="./rmh-container.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-container',
+
+  computed: {
+    hasDefaultSlot () {
+      return !!this.$slots.default
+    }
+  }
+}
+</script>
+
+<style src="./rmh-container.styl" lang="stylus"></style>
