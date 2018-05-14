1776b42e7f894807a0ecb2a7d3f11a0d352c73c8
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 20:41:10 +0200

    add: component rmh-drawer

---
+++ src/components/layout/rmh-drawer/rmh-drawer.vue
@@ -0,0 +1,38 @@
+<template src="./rmh-drawer.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-drawer',
+
+  props: {
+    right: {
+      type: Boolean,
+      default: false
+    },
+    open: {
+      type: Boolean,
+      default: false
+    },
+    persistent: {
+      type: Boolean,
+      default: false
+    },
+    wallClick: {
+      type: Function,
+      default: () => {
+        return null
+      }
+    }
+  },
+
+  computed: {
+    hasDefaultSlot () {
+      return !!this.$slots.default
+    }
+  }
+}
+</script>
+
+<style src="./rmh-drawer.styl" lang="stylus"></style>
