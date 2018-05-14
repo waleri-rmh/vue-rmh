25626c41dd5374465ee73da7ed1fa35052e3bb77
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 21:34:17 +0200

    add: component rmh-card

---
+++ src/components/layout/rmh-card/rmh-card.vue
@@ -0,0 +1,17 @@
+<template src="./rmh-card.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-card',
+
+  computed: {
+    hasDefaultSlot () {
+      return !!this.$slots.default
+    }
+  }
+}
+</script>
+
+<style src="./rmh-card.styl" lang="stylus"></style>
