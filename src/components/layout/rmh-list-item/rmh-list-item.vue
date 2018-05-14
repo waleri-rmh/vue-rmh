776676b45ba208e31d2e4237c548396ad4a21cd7
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-14 20:41:29 +0200

    add: component rmh-list; add: component rmh-list-item

---
+++ src/components/layout/rmh-list-item/rmh-list-item.vue
@@ -0,0 +1,18 @@
+<template src="./rmh-list-item.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-list-item',
+
+  props: {
+    link: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style src="./rmh-list-item.styl" lang="stylus"></style>
---
+++ src/components/layout/rmh-list/rmh-list.vue
@@ -0,0 +1,11 @@
+<template src="./rmh-list.html"></template>
+
+<script>
+import component from '../../../mixins/component'
+
+export default {
+  name: 'rmh-list'
+}
+</script>
+
+<style src="./rmh-list.styl" lang="stylus"></style>
