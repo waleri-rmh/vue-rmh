02041cbaa85f839fc2d5aefe270a7654e8805f83
Waleri Tumanez <w.tumanez@rmh.de>
2018-05-16 01:13:58 +0200

    fix: rmh-app fixed toolbar content selector

--- src/components/layout/rmh-app/rmh-app.vue
+++ src/components/layout/rmh-app/rmh-app.vue
@@ -35,14 +35,12 @@ export default {
 
   methods: {
     calculateContentHeight () {
-      if (this.fixedToolbar) {
-        const toolbar = document.querySelector('.rmh-app > .rmh-app-container > .rmh-toolbar')
-        const content = document.querySelector('.rmh-app > .rmh-app-container > .rmh-content')
-        if (toolbar && content) {
+      const toolbar = document.querySelector('.rmh-app > .rmh-app-container > .rmh-toolbar')
+      const content = document.querySelector('.rmh-app > .rmh-app-container > .rmh-content')
+      if (!content)
+        return
+      if (this.fixedToolbar && toolbar) {
           content.style.height = 'calc(100% - ' + toolbar.clientHeight + 'px)'
-        } else {
-          content.style.height = 'auto'
-        }
       } else {
         content.style.height = 'auto'
       }
