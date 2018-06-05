11af63918c80dfc26ede2134f9e228a2f055254a
Waleri Tumanez <w.tumanez@rmh.de>
2018-06-05 12:25:00 +0200

    fix: rmh-card action & content css

--- src/components/layout/rmh-card/rmh-card-action/rmh-card-action.vue
+++ src/components/layout/rmh-card/rmh-card-action/rmh-card-action.vue
@@ -6,9 +6,32 @@ import component from '@/mixins/component'
 export default {
   name: 'rmh-card-action',
 
+  props: {
+    alignRight: {
+      type: Boolean,
+      default: false
+    },
+    alignCenter: {
+      type: Boolean,
+      default: false
+    },
+    reversed: {
+      type: Boolean,
+      default: false
+    }
+  },
+
   computed: {
     hasDefaultSlot () {
       return !!this.$slots.default
+    },
+
+    classes () {
+      return {
+        'align-right': this.alignRight,
+        'align-center': this.alignCenter,
+        'reversed': this.reversed
+      }
     }
   }
 }
